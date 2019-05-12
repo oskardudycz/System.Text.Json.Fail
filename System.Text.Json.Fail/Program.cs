804a8ce0b659fa11538393cf538dce9a80b47558
Oskar Dudycz <oskar.dudycz@gmail.com>
2019-05-12 14:14:37 +0200

    Initial commit with fail scenario for System.Text.Json

---
+++ System.Text.Json.Fail/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using Marten.Testing;
+
+namespace System.Text.Json.Fail
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var document = new Target { Color = Colors.Blue, Number = 1 };
+            var str = JsonSerializer.ToString(document, new JsonSerializerOptions() { MaxDepth = 1 });
+        }
+    }
+}
